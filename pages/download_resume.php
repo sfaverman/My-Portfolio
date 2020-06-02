fd22aa7475b8b3bcfc6aa37adc36a1c7a08ba676
sfaverman <sofiasd@yahoo.com>
2020-06-02 15:01:52 -0700

    Added Visit-san-diego website to portfolio and resume touch--up

--- pages/download_resume.php
+++ pages/download_resume.php
@@ -1,6 +1,6 @@
 <?php
 $file_url = '/Resume_Sofia_Faverman.pdf';
 header('Content-Type: application/pdf');
-header("Content-disposition: attachment; filename=\"" . 'Sofia_Faverman_Resume.pdf' . "\"");
+header("Content-disposition: attachment; filename=\"" . 'Resume_Sofia_Faverman.pdf' . "\"");
 readfile($file_url);
 ?>
